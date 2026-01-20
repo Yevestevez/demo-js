8fc90e7e84cd8b3f2e0384ff3d169a423a00c8da
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-01-20 14:25:45 +0100

    Add exercise 9

---
+++ exercises/basics/ex9.js
@@ -0,0 +1,10 @@
+// 9. Crea una función que saque por consola el área de un rectángulo de dimensiones dadas como parámetro. (base, altura)
+
+const base = 5;
+const altura = 2;
+
+function area(base, altura) {
+    return base * altura;
+}
+
+console.log(area(base, altura));
