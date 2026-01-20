b9ffacef80be2d4de857c702f7ba2c9a8dbb5c00
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-01-20 14:16:09 +0100

    Add exercise 8

---
+++ exercises/basics/ex8.js
@@ -0,0 +1,9 @@
+// Crea una función que imprima por consola el resultado de elevar al cubo un número dado como parámetro.
+
+const a = 3;
+
+function cube(a) {
+    return a * a * a;
+}
+
+console.log(cube(a)); // 27
