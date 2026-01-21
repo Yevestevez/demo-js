87918d18e86fc4ee5722e4e4f61ab6ac5afb005f
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-01-21 13:18:27 +0100

    Update exersice 10 including for loop

--- exercises/basics/ex10.js
+++ exercises/basics/ex10.js
@@ -1,7 +1,11 @@
 // 10. Crea una función que imprima por consola un número al azar entre 0 y 10.
 
-function randomNum(max) {
-    return Math.floor(Math.random() * max);
+function randomNum() {
+    return Math.floor(Math.random() * 11);
 }
 
-console.log(randomNum(11));
+for (let i = 1; i <= 10; i++) {
+    console.log(i, randomNum());
+}
+
+//console.log(randomNum());
