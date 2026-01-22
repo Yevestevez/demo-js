4269987ad474965b9f2c83a15c3c1dd106f54ba1
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-01-22 10:55:04 +0100

    Update exercise 12 with better functions

--- exercises/basics/ex12.js
+++ exercises/basics/ex12.js
@@ -1,11 +1,12 @@
 // 12. Crea una función que reciba un número como parámetro e imprima por consola si el número dado es par o impar.
 
-const num = 6;
+const num = 5;
 
-function pairOrEven(num) {
+// v1. Función básica con if/else
+function oddOrEven(num) {
     let result;
 
-    if ((num & 1) === 0) {
+    if (num % 2 === 0) {
         result = 'par';
     } else {
         result = 'impar';
@@ -14,4 +15,36 @@ function pairOrEven(num) {
     return result;
 }
 
-console.log(`el número ${num} es`, pairOrEven(num));
+// v2. Función básica con if/else optimizada
+function oddOrEven2(num) {
+    let result = 'impar';
+
+    if (num % 2 === 0) {
+        result = 'par';
+    }
+
+    return result;
+}
+
+// v3. Operador ternario
+function oddOrEven3(num) {
+    let result = num % 2 === 0 ? 'par' : 'impar';
+    return result;
+}
+
+// v4. Función flecha
+const oddOrEven4 = (num) => {
+    if (num % 2 === 0) {
+        return 'par';
+    } else {
+        return 'impar';
+    }
+};
+
+const oddOrEven5 = (num) => (num % 2 === 0 ? 'par' : 'impar'); // v5. Operador ternario + función flecha
+
+console.log(`v1: el número ${num} es`, oddOrEven(num));
+console.log(`v2: el número ${num} es`, oddOrEven2(num));
+console.log(`v3: el número ${num} es`, oddOrEven3(num));
+console.log(`v4: el número ${num} es`, oddOrEven4(num));
+console.log(`v5: el número ${num} es`, oddOrEven5(num));
