f3914b42e8be0939711b4a47c87d91268dbdb557
Yevestevez <yevestevez.cg@gmail.com>
2026-02-12 18:03:27 +0100

    Add exercise 13 medium empty

---
+++ exercises/medium/ex13.js
@@ -0,0 +1,1 @@
+// Escribe una función generadora de nombres de usuario aleatorios, a partir de dos grupos de palabras dadas. Estos grupos de palabras pueden estar agrupados en arrays. (nombres=['Hugo', 'Luis'], apellidos=['Duro', 'Fabiano']). Retorna un nombre de usuario aleatorio con nombre, apellido y un número aleatorio del 1 al 100. (Por ejemplo -> 'Pepe Pérez 87'.)
